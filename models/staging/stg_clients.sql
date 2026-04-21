c4bc832062b629f849df18cfedb9da402d5e1c1c
jeanne.eichelbrenner@student.lacapsule.academy <jeanne.eichelbrenner@student.lacapsule.academy>
2026-04-21 13:23:06 +0000

    nettoyage nulls table clients

---
+++ models/staging/stg_clients.sql
@@ -0,0 +1,12 @@
+{{
+    config(materialized='table')
+}}
+    
+    SELECT
+id_client,
+nom_client,
+prenom_client,
+email_client,
+id_segment
+FROM `vtc-paris-493907.modele_en_etoile.clients`
+WHERE email_client IS NOT NULL
