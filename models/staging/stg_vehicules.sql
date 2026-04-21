fdba5b7b2e6b6d68421e093e7c21b69384a20da2
jeanne.eichelbrenner@student.lacapsule.academy <jeanne.eichelbrenner@student.lacapsule.academy>
2026-04-21 13:40:59 +0000

    update nom de table source

--- models/staging/stg_vehicules.sql
+++ models/staging/stg_vehicules.sql
@@ -5,7 +5,7 @@
 SELECT
 id_vehicule,
 modele_vehicule,
-id_type_vehicule,
-immatriculation
-FROM `centralisation_donnees_vtc.vehicules`
+immatriculation,
+id_type_vehicule
+FROM `vtc-paris-493907.modele_en_etoile.vehicules`
 WHERE immatriculation IS NOT NULL
