44c826f3b4441c2cd4cb2fe6f157ca0a1672b8b2
Jeanne Eichelbrenner <jeanne.eichelbrenner@student.lacapsule.academy>
2026-06-13 11:09:28 +0000

    stg_horaires_2023 ok

--- models/transports_IDFM/staging/stg_horaires_2023.sql
+++ models/transports_IDFM/staging/stg_horaires_2023.sql
@@ -1,24 +1,49 @@
+WITH source_data AS (
+    SELECT *
+    FROM {{ source('idfm_raw', 'horaires_2023') }}
+),
+
+
+deduplicated AS (
+    SELECT *
+    FROM (
+        SELECT
+            *,
+            row_number() OVER (
+                PARTITION BY date, heure, ligne_id
+                ORDER BY ligne_nom
+            ) AS row_num
+        FROM source_data
+    )
+    WHERE row_num = 1
+),
+
+clean_data as (
 SELECT
-    date,
-    annee,
-    mois,
-    jour,
-    jour_semaine,
-    CAST(heure AS STRING) AS heure,
-    tranche_horaire,
-    id_ligne_IDFM AS id_ligne_idfm,
-    ligne_nom,
-    type_transport,
-    frequence_theorique_par_heure,
-    frequence_reelle_par_heure,
-    taux_service_pct,
-    retard_moyen_minutes,
-    facteur_retard,
-    incident_detecte,
-    incident_type,
-    temperature,
-    precipitation,
-    neige,
-    vent,
-    meteo_defavorable
-FROM {{ source('idfm_raw', 'horaires_2023') }}
+    cast(date as date) as date,
+    cast(annee as int64) as annee,
+    cast(mois as int64) as mois,
+    cast(jour as int64) as jour,
+    cast(jour_semaine as string) as jour_semaine,
+    cast(heure as int64) as heure,
+    cast(tranche_horaire as string) as heure_pointe,
+    split(cast(ligne_id as string), ':')[OFFSET(1)] as id_ligne_idfm,
+    cast(ligne_nom as string) as libelle_ligne,
+    cast(type_transport as string) as type_transport,
+    cast(frequence_theorique_par_heure as float64) as frequence_theorique,
+    cast(frequence_reelle_par_heure as float64) as frequence_reelle,
+    cast(taux_service_pct as float64) as taux_service_pct,
+    cast(retard_moyen_minutes as float64) as retard_moyen_minutes,
+    cast(facteur_retard as float64) as facteur_retard,
+    cast(incident_detecte as bool) as incident_detected,
+    cast(incident_type as string) as incident_type,
+    cast(temperature as float64) as temperature,
+    cast(precipitation as float64) as preceipitation,
+    cast(neige as float64) as neige,
+    cast(vent as float64) as vent,
+    cast(meteo_defavorable as bool) as meteo_defavorable
+FROM deduplicated
+)
+
+SELECT *
+FROM clean_data
