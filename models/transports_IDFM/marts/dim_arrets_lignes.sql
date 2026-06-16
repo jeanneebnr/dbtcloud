4462fb421122bfc9e93b6a3c49ad766bb5cdf1e0
Jeanne Eichelbrenner <jeanne.eichelbrenner@student.lacapsule.academy>
2026-06-16 17:53:13 +0000

    dim ok

--- models/transports_IDFM/marts/dim_arrets_lignes.sql
+++ models/transports_IDFM/marts/dim_arrets_lignes.sql
@@ -1,23 +1,15 @@
 {{ config(materialized='table') }}
 
 SELECT DISTINCT
-
     al.id_stop_idfm AS id_arret_zda,
-
     ref.id_zdc AS id_arret_zdc,
-
     dl.id_ligne
-
 FROM {{ ref('stg_arrets_lignes') }} al
-
 LEFT JOIN {{ ref('stg_arrets_referentiel') }} ref
     ON al.id_stop_idfm = ref.id_arret_idfm
-
 LEFT JOIN {{ ref('dim_lignes') }} dl
     ON al.id_ligne_idfm = dl.id_ligne_idfm
-
 WHERE
     al.id_stop_idfm IS NOT NULL
     AND dl.id_ligne IS NOT NULL
-
 ORDER BY id_arret_zdc DESC
--- models/transports_IDFM/marts/dim_liste_transporteurs.sql
+++ models/transports_IDFM/marts/dim_liste_transporteurs.sql
@@ -1,20 +1,19 @@
 {{ config(materialized='table') }}
 
 SELECT
-    ROW_NUMBER() OVER (ORDER BY operatorref) AS id_operateur,
-    operatorname AS libelle_transporteur,
-    operatorref AS transporteur_ref,
-    housenumber AS num_rue,
-    street AS rue,
-    addressline1 AS adresse_ligne_1,
-    town AS ville,
-    postcode AS code_postal,
-    postcodeextension AS code_postal_extension,
-    phone AS tel,
-    url,
-    furtherdetails AS detail,
-    contactperson AS contact,
-    logo,
-    email
-
+    ROW_NUMBER() OVER (ORDER BY transporteur_ref) AS id_operateur,
+    libelle_transporteur AS libelle_transporteur,
+    transporteur_ref AS transporteur_ref,
+    numero_rue AS numero_rue,
+    rue AS rue,
+    adresse_ligne_1 AS adresse_ligne_1,
+    ville AS ville,
+    code_postal AS code_postal,
+    cp_extension AS code_postal_extension,
+    telephone AS telephone,
+    url AS url,
+    details AS details,
+    contact AS contact,
+    logo AS logo,
+    email AS email
 FROM stg_liste_transporteurs
