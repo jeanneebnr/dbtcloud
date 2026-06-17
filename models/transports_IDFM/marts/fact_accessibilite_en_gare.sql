e917eca6a94c09b00a87c013ced5e5196a191c61
Jeanne Eichelbrenner <jeanne.eichelbrenner@student.lacapsule.academy>
2026-06-17 23:08:37 +0000

    fact ok

--- models/transports_IDFM/marts/fact_accessibilite_en_gare.sql
+++ models/transports_IDFM/marts/fact_accessibilite_en_gare.sql
@@ -3,7 +3,7 @@
 SELECT 
     al.id_stop_idfm as id_stop_zda,
     aeg.niveau_accessibilite,
-    aeg.note_accessibilite
+    aeg.nom_accessibilite
 
 FROM {{ref('stg_accessibilite_en_gare')}} as aeg
 LEFT JOIN {{ref('stg_arrets_lignes')}} as al
--- models/transports_IDFM/marts/fact_trafic_heure.sql
+++ models/transports_IDFM/marts/fact_trafic_heure.sql
@@ -10,14 +10,14 @@ SELECT
     ROW_NUMBER() OVER (ORDER BY CAST(uh.date AS DATE), CAST(uh.heure AS INT64)) AS id_trafic,
     CAST(uh.date AS DATE)AS date,
     CAST(uh.heure AS INT64)AS heure,
-    CAST(uh.tranche_horaire AS STRING) AS tranche_horaire,
+    CAST(uh.heure_pointe AS STRING) AS tranche_horaire,
     CAST(dl.id_ligne AS STRING) AS id_ligne,
-    CAST(uh.frequence_theorique_par_heure AS FLOAT64) AS freq_theo,
-    CAST(uh.frequence_reelle_par_heure AS FLOAT64) AS freq_reel,
+    CAST(uh.frequence_theorique AS FLOAT64) AS freq_theo,
+    CAST(uh.frequence_reelle AS FLOAT64) AS freq_reel,
     CAST(uh.taux_service_pct AS FLOAT64) AS taux_service_pct,
     CAST(uh.retard_moyen_minutes AS FLOAT64) AS retard_moyen_minute,
     CAST(uh.facteur_retard AS FLOAT64) AS facteur_retard,
-    CAST(uh.incident_detecte AS BOOL) AS incident_detecte,
+    CAST(uh.incident_detected AS BOOL) AS incident_detecte,
     CAST(uh.incident_type AS STRING) AS incident_type
 
 FROM union_horaires as uh
